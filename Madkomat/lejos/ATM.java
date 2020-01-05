617fb3670885071690b449355084a15b3017dfbf
RomekRJM <romek.rjm@gmail.com>
2020-01-05 19:40:14 +0100

    Fix servos moving logic.

--- Madkomat/lejos/ATM.java
+++ Madkomat/lejos/ATM.java
@@ -1,16 +1,17 @@
 import lejos.nxt.Button;
 import lejos.nxt.ButtonListener;
 import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
 
 public class ATM implements ButtonListener {
 
     private static final int HATCH_MOVEMENT_SPEED = 540;
     private static final int HATCH_MOVEMENT_DURATION = 300;
-    private static final Motor HATCH_MOTOR = Motor.A;
+    private static final NXTRegulatedMotor HATCH_MOTOR = Motor.A;
 
-    private static final int BANKNOTE_ROLLER_MOVEMENT_SPEED = 540;
-    private static final int BANKNOTE_ROLLER_MOVEMENT_DURATION = 200;
-    private static final Motor BANKNOTE_ROLLER_MOTOR = Motor.C;
+    private static final int BANKNOTE_ROLLER_MOVEMENT_SPEED = 360;
+    private static final int BANKNOTE_ROLLER_MOVEMENT_DURATION = 800;
+    private static final NXTRegulatedMotor BANKNOTE_ROLLER_MOTOR = Motor.C;
 
     public void openHatch() {
         rotateMotor(HATCH_MOTOR, HATCH_MOVEMENT_SPEED, false, HATCH_MOVEMENT_DURATION);
@@ -25,10 +26,22 @@ public class ATM implements ButtonListener {
                 BANKNOTE_ROLLER_MOVEMENT_DURATION);
     }
 
+    public void rotateMotor(NXTRegulatedMotor motor, int speed, boolean forward, long timeInMilis) {
+        motor.setSpeed(speed);
+        if (forward) {
+            motor.forward();
+        } else {
+            motor.backward();
+        }
+        sleepCalmly(timeInMilis);
+        motor.stop();
+    }
+
     public void buttonPressed(Button button) {
         if (Button.ENTER.equals(button)) {
             openHatch();
             passBanknotes();
+            sleepCalmly(1000);
             closeHatch();
         }
     }
@@ -45,7 +58,7 @@ public class ATM implements ButtonListener {
 
     public static void main(String[] args) {
         ATM atm = new ATM();
-        Button.ENTER.addButtonListener(madkomat);
+        Button.ENTER.addButtonListener(atm);
         Button.ESCAPE.waitForPressAndRelease();
     }
 }
