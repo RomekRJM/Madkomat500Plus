ca85b1a73fd2a68f736bac143720235bbe716465
Roman Subik <roman.subik@ocado.com>
2020-03-07 21:56:05 +0100

    Changing imagePreview size/margins.

--- MadkomatApp/app/src/main/java/com/example/madkomatapp/MainActivity.java
+++ MadkomatApp/app/src/main/java/com/example/madkomatapp/MainActivity.java
@@ -13,7 +13,9 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -230,6 +232,10 @@ public class MainActivity extends AppCompatActivity implements AnimationListener
 
             Bitmap bitmap = CameraUtils.optimizeBitmap(imageStoragePath);
             imgPreview.setBackgroundImage(bitmap);
+
+            ViewGroup.MarginLayoutParams layoutParams = new ViewGroup.MarginLayoutParams(bitmap.getWidth(), bitmap.getHeight());
+            layoutParams.setMargins(0,0,0,0);
+            imgPreview.setLayoutParams(new LinearLayout.LayoutParams(layoutParams));
 
         } catch (NullPointerException e) {
             e.printStackTrace();
